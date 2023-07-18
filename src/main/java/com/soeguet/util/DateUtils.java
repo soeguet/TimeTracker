c073625bc60a7304e213fd21d4a152a7e798d3a9
soeguet <soeguet.osman@gmail.com>
2023-07-18 11:24:37 +0200

    add -- util function convert Duration to String (neg && pos respectively)

--- src/main/java/com/soeguet/util/DateUtils.java
+++ src/main/java/com/soeguet/util/DateUtils.java
@@ -1,9 +1,26 @@
 package com.soeguet.util;
 
+import java.time.Duration;
 import java.time.LocalDate;
 
 public class DateUtils {
   public static boolean isFuture(LocalDate date) {
     return date != null && date.isAfter(LocalDate.now());
+  }
+
+  public static String convertDurationToString(Duration duration) {
+    boolean neg = false;
+    if (duration.isNegative()){
+      neg = true;
+      duration = duration.multipliedBy(-1);
+    }
+    long seconds = duration.getSeconds();
+    int minutes = (int) seconds / 60;
+    int hours = minutes / 60;
+
+    if (neg){
+      return String.format("-%02d:%02d", hours, minutes % 60);
+    }
+    return String.format("%02d:%02d", hours, minutes % 60);
   }
 }
